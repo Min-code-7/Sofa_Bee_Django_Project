2b85b07b2c0a65617fd9f03b4491069b14cd52fe
郑砚柯 <451044532@qq.com>
2025-03-17 20:05:10 +0000

    只是功能

--- Sofa_Bee_Django_Project/urls.py
+++ Sofa_Bee_Django_Project/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
+    path('', home, name='home'),  # 添加主页
+    path('orders/', include('orders.urls')),  # 添加订单管理的 URL
 ]
