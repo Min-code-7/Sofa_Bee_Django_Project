8c93b259e937a7f9ddd79d6f8f523b8c4749f2e4
min <min@mindeMacBook-Pro.local>
2025-03-20 04:05:49 +0000

    add lass settings about email

--- Sofa_Bee_Django_Project/settings.py
+++ Sofa_Bee_Django_Project/settings.py
@@ -138,3 +138,13 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.163.com'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'itforpurchasing@163.com'
+EMAIL_HOST_PASSWORD = 'JUGh78SjzvQXKwr4'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
