d13bb41260074e394f6ae13a67668b89ca011430
kaka <3022378X@student.gla.ac.uk>
2025-03-20 13:17:04 +0000

    fix problem of home and css

--- Sofa_Bee_Django_Project/views.py
+++ Sofa_Bee_Django_Project/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import redirect
 
 def home(request):
-    return HttpResponse("<h1>Welcome to Sofa Bee Order Management System!</h1>")
+    # Redirect to products page
+    return redirect('products:product_list')
